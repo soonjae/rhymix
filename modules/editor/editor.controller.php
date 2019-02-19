c11011fdebbf8cd050bf733f26957b6edeb3883f
Soonjae Kim <soonj@nate.com>
2019-02-19 10:36:45 +0900

    이미지 본문 삽입 기능을 각 모듈별로 설정하게...
    editor.controller.php와 tpl/editor_module_config.html 을 수정

--- modules/editor/editor.controller.php
+++ modules/editor/editor.controller.php
@@ -157,6 +157,7 @@ class editorController extends editor
 		$editor_config->comment_editor_height = (int)Context::get('comment_editor_height');
 		$editor_config->enable_autosave = Context::get('enable_autosave') ?: 'Y';
 		$editor_config->allow_html = Context::get('allow_html') ?: 'Y';
+		$editor_config->autoinsert_image = Context::get('autoinsert_image') ?: 'paragraph';
 
 		$oModuleController = getController('module');
 		foreach ($module_srl as $srl)
